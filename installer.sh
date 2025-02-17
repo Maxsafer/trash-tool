ce622b53a4f9f3a653956bce151e907c7816625b
Max <42416882+Maxsafer@users.noreply.github.com>
2025-02-17 08:30:43 -0600

    Updated installation order and permissions

--- installer.sh
+++ installer.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-mkdir trash_tool
-chmod 775 trash_tool
+# Detect Python command
+if command -v python &> /dev/null; then
+    PYTHON_CMD="python"
+elif command -v python3 &> /dev/null; then
+    PYTHON_CMD="python3"
+else
+    echo "Python is not installed. Please install Python to continue."
+    exit 1
+fi
+
+# Check Python version
+PYTHON_VERSION="$($PYTHON_CMD -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')"
+REQUIRED_VERSION="2.6.6"
+
+if [ "$(printf '%s\n' "$REQUIRED_VERSION" "$PYTHON_VERSION" | sort -V | head -n1)" != "$REQUIRED_VERSION" ]; then 
+    echo "Warning: Python version $REQUIRED_VERSION or higher is required. Current version is $PYTHON_VERSION"
+    exit 1
+else
+    echo "Python version $PYTHON_VERSION detected, installed version meets the requirement."
+fi
+
+# Create directory with user-only permissions
+mkdir -m 700 trash_tool
 cd trash_tool
 
 toolDir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
+# Download and set execute permissions for the script
 curl https://raw.githubusercontent.com/Maxsafer/trash-tool/refs/heads/main/trash.sh -o trash.sh
-chmod u+x trash.sh
-chmod 775 trash.sh
+chmod 700 trash.sh
 
 # Check if trash.py exists and delete it if it does
 if [ -f "trash.py" ]; then
@@ -42,27 +63,7 @@ if ! grep -q "alias trash=" "$SHELL_CONFIG_FILE"; then
     echo "alias trash='$toolDir/trash.sh'" >> "$SHELL_CONFIG_FILE"
     echo "alias ts='$toolDir/trash.sh'" >> "$SHELL_CONFIG_FILE"
     source $SHELL_CONFIG_FILE
-    echo "Alias 'trash' and 'ts' added to $SHELL_CONFIG_FILE. Sourcing $SHELL_CONFIG_FILE... Use trash -h for help."
+    echo "Alias 'trash' and 'ts' added to $SHELL_CONFIG_FILE. Please use 'source $SHELL_CONFIG_FILE' to start using trash."
 else
     echo "Trash alias already exists in $SHELL_CONFIG_FILE"
-fi
-
-# Detect Python command
-if command -v python &> /dev/null; then
-    PYTHON_CMD="python"
-elif command -v python3 &> /dev/null; then
-    PYTHON_CMD="python3"
-else
-    echo "Python is not installed. Please install Python to continue."
-    exit 1
-fi
-
-# Check Python version
-PYTHON_VERSION="$($PYTHON_CMD -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')"
-REQUIRED_VERSION="2.6.6"
-
-if [ "$(printf '%s\n' "$REQUIRED_VERSION" "$PYTHON_VERSION" | sort -V | head -n1)" = "$REQUIRED_VERSION" ]; then 
-    echo "Python version $PYTHON_VERSION detected, installed version meets the requirement."
-else
-    echo "Warning: Python version $REQUIRED_VERSION or higher is required. Current version is $PYTHON_VERSION"
 fi
